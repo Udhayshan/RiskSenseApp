8dad81149f6715a375175daf2b608515d30715c5
Udhay Shankar <ushankar@marketo.com>
2018-10-27 22:09:10 +0530

    Server code for search functionality

--- jobsearchportal/server/index.js
+++ jobsearchportal/server/index.js
@@ -4,9 +4,18 @@ const filters = require('./filters.json')
 const cors = require('cors')
 const app = express().use(cors());
 
+const byTitle = title => jobTitle =>
+  jobTitle.title.toLowerCase().includes(title.toLowerCase())
+
 app.get("/jobs", (req, res, next) => {
   res.status(200).json(jobs)
  });
+
+app.get('/jobs/:title', (req, res) => {
+  res.status(200).json(
+    jobs.jobs.filter(byTitle(req.params.title))
+  ) 
+});
 
 app.get("/filters", (req, res, next) => {
   res.status(200).json(filters)
